95cab7f3e3406730f25bee2e6281e87433732973
TannerWaltz <tannerwaltz99@gmail.com>
2023-04-24 10:53:54 -0400

    added natural color

--- FinalProject.py
+++ FinalProject.py
@@ -5,14 +5,16 @@ import vtk.util.numpy_support as vtk_np
 # import open3d as o3d
 
 
-pc = laspy.read('Mawchu_LLacta_UTM.las')
+pc = laspy.read('C:/Users/tanne/Desktop/VisualizationFinalProjectData/Mawchu_LLacta_UTM.las')
 # pc = laspy.read('2.las')
 
 pc_array = np.vstack([pc.x, pc.y, pc.z]).transpose()
-pc_array = pc_array[::100] # Randomly reducing the points 
+# pc_array = pc_array[::100] # Randomly reducing the points 
                         #  by a factor of 100
-# print(pc_array.shape)
-# print(pc_array[0])
+print(pc_array.shape)
+print(pc_array[0])
+
+colors = np.vstack([pc.red/2**16, pc.green/2**16, pc.blue/2**16]).transpose()
 
 nCoords = pc_array.shape[0]
 nElem = pc_array.shape[1]
@@ -27,20 +29,21 @@ verts.SetData(vtk_np.numpy_to_vtk(pc_array))
 
 cells_npy = np.vstack([np.ones(nCoords,dtype=np.int64),
                 np.arange(nCoords,dtype=np.int64)]).T.flatten()
-# print(cells_npy.shape)
+print(cells_npy.shape)
 
 cells.SetCells(nCoords,vtk_np.numpy_to_vtkIdTypeArray(cells_npy))
 
 pd.SetPoints(verts)
 pd.SetVerts(cells)
+pd.GetPointData().SetScalars(vtk_np.numpy_to_vtk(colors))
 
 mapper = vtk.vtkPolyDataMapper()
 mapper.SetInputDataObject(pd)
+mapper.SetColorModeToDirectScalars()
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
 actor.GetProperty().SetRepresentationToPoints()
-actor.GetProperty().SetColor(0.0,1.0,0.0) 
 
 ren = vtk.vtkRenderer()
 ren.AddActor(actor)
